3c139800c82424630a644eb305b0ea90f36e88a1
ahmedmeshref <a.meshref@alustudent.com>
2020-08-21 02:37:58 +0200

    Add py sol

---
+++ day_20/Reorder_List.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        curr = head
+        nodes = []
+        while curr:
+            nodes.append(curr)
+            curr = curr.next
+
+        curr = head
+        while curr and curr.next:
+            last_node = nodes.pop()
+            if last_node is not curr.next:
+                nodes[-1].next = None
+                last_node.next = curr.next
+                curr.next = last_node
+                curr = last_node.next
+            else:
+                curr = curr.next
+        return head
+
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+s = Solution()
+
+head = s.reorderList(head)
+while head:
+    print(head.val)
+    head = head.next
